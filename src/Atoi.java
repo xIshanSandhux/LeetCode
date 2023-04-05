25a34b57bcd03800e94b6f343d8ecc09d2aa8b09
IPSSandhu <ishan3121@uvic.ca>
2023-04-04 17:28:21 -0700

    Initial commit - with Question 8 Solved

---
+++ src/Atoi.java
@@ -0,0 +1,57 @@
+/*
+*  LeetCode
+*   Question Number:- 8
+*   Difficulty:- Medium
+ */
+public class Atoi {
+    public static int myAtoi(String s) {
+
+        long number = 0;
+        char[] chars = s.toCharArray();
+        int i = 0;
+        double sign = 1;
+        char current = chars[i];
+
+        while(current == ' '){
+            i++;
+            if(i == s.length()){
+                return 0;
+            }
+            current = chars[i];
+
+        }
+        if(chars[i]=='-'){
+            sign = -1;
+            i++;
+        }else if(chars[i]=='+'){
+            sign = 1;
+            i++;
+        }
+
+        for(;i<s.length();i++){
+            current = chars[i];
+            if(current<'0' || current>'9'){
+
+                break;
+            }else{
+
+                number *=10;
+                number +=current - '0';
+
+                if(number==23){
+                    System.out.println(i);
+
+                }
+
+                if(sign*number<=Integer.MIN_VALUE){
+                    return Integer.MIN_VALUE;
+                }
+                if(sign*number>=Integer.MAX_VALUE){
+                    return Integer.MAX_VALUE;
+                }
+            }
+        }
+        int r = (int)(number * sign);
+        return r;
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+
+    public static void leetcode_8(String s){
+        int atoi = Atoi.myAtoi(s);
+        System.out.println(atoi);
+    }
+
+    public static void main(String[] args) {
+        leetcode_8("4193 with words");
+
+    }
+}
